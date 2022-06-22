a562e1ac7e40508337b42602e3d1bfecdbe4488a
Justin Roberts <justinroberts@Justins-MacBook-Pro.local>
2022-06-22 13:14:01 -0400

    trying to fix build

---
+++ build/precache-manifest.3b14f7b45714619388b6f2d15aa2d6f2.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e83398ba185499f785ee2b3869bfe0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "754f17bbe7cbf91fcaff",
+    "url": "/static/css/main.4131be53.chunk.css"
+  },
+  {
+    "revision": "b89436dd16ed68e43cf6",
+    "url": "/static/js/2.5917daf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5917daf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754f17bbe7cbf91fcaff",
+    "url": "/static/js/main.54ae154a.chunk.js"
+  },
+  {
+    "revision": "2da695eeeb1e7e863336",
+    "url": "/static/js/runtime-main.c99ad947.js"
+  },
+  {
+    "revision": "d10d0df4e7fa1ebd5435b43a11bb2575",
+    "url": "/static/media/Akir_with_roses_for_Pink_Bandanazz_Brooklyn_April_2019.d10d0df4.mp4"
+  },
+  {
+    "revision": "cabe6ffdb8fa53ded20afc1553ee7da8",
+    "url": "/static/media/Alfie_in_the_corset_Brooklyn_March_2019.cabe6ffd.m4a"
+  },
+  {
+    "revision": "c440c95706aa09ea7136c59266a87da8",
+    "url": "/static/media/Andi_after_the_protest_Brooklyn_November_2019.c440c957.m4a"
+  },
+  {
+    "revision": "1bfaf424e05a14bf4adf21b67ce08b0b",
+    "url": "/static/media/Andi_on_the_couch_Punta_Negra_March_2020.1bfaf424.m4a"
+  },
+  {
+    "revision": "4d8fdeb265ff6d34fb3bd8e4292665c0",
+    "url": "/static/media/AvenirNext-Regular.4d8fdeb2.ttf"
+  },
+  {
+    "revision": "88da9ccecc9a384876ccf7c53eccec2d",
+    "url": "/static/media/AvenirNextBold.88da9cce.ttf"
+  },
+  {
+    "revision": "db5edae5fe203454c7a09325da4c35c9",
+    "url": "/static/media/Continua-Regular.db5edae5.otf"
+  },
+  {
+    "revision": "6da0430e2ca57c953b771027ee7e15e6",
+    "url": "/static/media/Cristobal_as_Buffy_on_Halloween_October_2019.6da0430e.m4a"
+  },
+  {
+    "revision": "bf560eeed5702cd57475368832f98efb",
+    "url": "/static/media/Djino,_fire_escape,_lip_gloss,_December_2019 (1).bf560eee.m4a"
+  },
+  {
+    "revision": "a27f2604e7ae298169c1a024524f2cb8",
+    "url": "/static/media/Domingos_new_home_January_2020.a27f2604.m4a"
+  },
+  {
+    "revision": "5b4c994f79ba1cd99c61aef00bcff7a4",
+    "url": "/static/media/Edua_after_their_performance_Queens_March_2019.5b4c994f.mp4"
+  },
+  {
+    "revision": "67245f2af3e5a210a50d8d751a0422b6",
+    "url": "/static/media/Fudge_at_her_shoot_Brooklyn_April_2019.67245f2a.mp4"
+  },
+  {
+    "revision": "9f732d4336460585d875a1b47282f373",
+    "url": "/static/media/Hermes-Bold.9f732d43.otf"
+  },
+  {
+    "revision": "1212e42465f6ecc97ef5202858fcc381",
+    "url": "/static/media/Hermes-Light.1212e424.otf"
+  },
+  {
+    "revision": "ede2254696ea086ab00c52bfb141b4b1",
+    "url": "/static/media/Iyabo_on_the_night_of_her_performance_Brooklyn_September_2019.ede22546.m4a"
+  },
+  {
+    "revision": "7f4835117ff375bddc28ff62eb316815",
+    "url": "/static/media/Jess_&_Lolu_in_the_morning_Brooklyn_November_2019.7f483511.m4a"
+  },
+  {
+    "revision": "92664d960559ad95e236ed5b76ef6e0b",
+    "url": "/static/media/Jess_and_Ocean_on_my_purple_sheets_Brooklyn_November_2019.92664d96.m4a"
+  },
+  {
+    "revision": "aa608a066c58308fa39506b77b64420c",
+    "url": "/static/media/Julio_in_my_hoops_Lima_May_2020.aa608a06.mp4"
+  },
+  {
+    "revision": "80c239255b2526673f52b73896a747e0",
+    "url": "/static/media/Karen_y_periodico_Lima_June_2019.80c23925.mp4"
+  },
+  {
+    "revision": "4868fac2ab9b4391c2ca3e546afa9ca4",
+    "url": "/static/media/Keioui_&_the_sea_at_Cherry_Grove_Fire_Island_May_2019 .4868fac2.m4a"
+  },
+  {
+    "revision": "f8f7762136594d7653ba46a735b9b814",
+    "url": "/static/media/Keiouis_Peanut_Stew_Brooklyn_November_2018.f8f77621.m4a"
+  },
+  {
+    "revision": "e186cdeb71c326a6bbd72566b6d15cd2",
+    "url": "/static/media/Lolu_smoke_break_Brooklyn_December_2019.e186cdeb.mp4"
+  },
+  {
+    "revision": "5e50540ce64a7ff6beb746de02414dac",
+    "url": "/static/media/Maria_al_lado_de_su_casa_de_niña_El_Carmen_February_2020.5e50540c.mp4"
+  },
+  {
+    "revision": "acade2f2eb9d287e802e306a82e0d1f0",
+    "url": "/static/media/Midrene_the_night_we_all_wore_berets_to_the_bar_Brooklyn_February_2018.acade2f2.m4a"
+  },
+  {
+    "revision": "1bf78e85ec9fc1fba26d9e0468f28712",
+    "url": "/static/media/Midrene_with_roses_Brooklyn_December_2017.1bf78e85.m4a"
+  },
+  {
+    "revision": "48aa63cf9d462d932cfadca6a8171a78",
+    "url": "/static/media/Naila_Mami_on_the_roof_Brooklyn_March_2019.48aa63cf.m4a"
+  },
+  {
+    "revision": "8aa15e55b6c579806e38ac95f6b43f72",
+    "url": "/static/media/Nancy_before_Libre_Brooklyn_November_2019.8aa15e55.mp4"
+  },
+  {
+    "revision": "919bfef485a1cf4e9985612604803114",
+    "url": "/static/media/Nancy_cigarette_Cherry_Grove_May_2019.919bfef4.mp4"
+  },
+  {
+    "revision": "093e13d01605d884782ede13b3ff380a",
+    "url": "/static/media/Nani_at_the_Lotus_Club_Reunion_DC_December_2019.093e13d0.m4a"
+  },
+  {
+    "revision": "59273de1cc40a08dd41e5cc38aa45089",
+    "url": "/static/media/Nitty_in_Burgundy_DC_December_2019.59273de1.m4a"
+  },
+  {
+    "revision": "06a698aa6c816cab229fa71a3395ec7d",
+    "url": "/static/media/Omololus_first_time_at_Riis_July_2019.06a698aa.mp4"
+  },
+  {
+    "revision": "1789d9f8b1ca992229023601872ab925",
+    "url": "/static/media/Oscar_red_nails_Brooklyn_March_2019.1789d9f8.mp4"
+  },
+  {
+    "revision": "e162b55e0847424ea947d0fef2f3b4bd",
+    "url": "/static/media/Pao_at_the_Tropicuir_Fundraiser_Brooklyn_November_2019.e162b55e.m4a"
+  },
+  {
+    "revision": "a3c22a0554493f417e8b54811b836532",
+    "url": "/static/media/Pedro_con_abanico_despues_de_bailar_Callao_Febrero_2020.a3c22a05.ogg"
+  },
+  {
+    "revision": "43c460e55f026855890577628f90e402",
+    "url": "/static/media/Prince_en_cuarentena_Lima_Abril_2020.43c460e5.mp4"
+  },
+  {
+    "revision": "f191460759c866d73d4872815fcac1e4",
+    "url": "/static/media/Prince_reaching_Lima_Abril_2020.f1914607.m4a"
+  },
+  {
+    "revision": "01fa988414510feb9c31263e2dff656d",
+    "url": "/static/media/Sandra_at_the_window_Lima_Marzo_2020.01fa9884.m4a"
+  },
+  {
+    "revision": "73a7e0d02703509435602e066691db5e",
+    "url": "/static/media/Sandra_en_naranja_Lima_Marzo_2020.73a7e0d0.m4a"
+  },
+  {
+    "revision": "0b5ea4ffd8d2773ce57c5b2b7f368a8c",
+    "url": "/static/media/Seraah_between_the_matriarchs_Boston_November_2010.0b5ea4ff.wav"
+  },
+  {
+    "revision": "5c1784624643ae2b80069d82f19bdac3",
+    "url": "/static/media/Sofia_en_el_techo_Brooklyn_June_2019.5c178462.mp4"
+  },
+  {
+    "revision": "16adbc8dc843120b18baa99f8eee44cb",
+    "url": "/static/media/Sonny_at_his_temporary_home_Brooklyn_January_2019.16adbc8d.m4a"
+  },
+  {
+    "revision": "870ff6cc6a13b393dbfc8d7648b6159d",
+    "url": "/static/media/Stefa_after_the_protest,_before_the_art_gala_Brooklyn_November_2019.870ff6cc.mp4"
+  },
+  {
+    "revision": "fc95b23567b6e4337d356f5d5f405f4d",
+    "url": "/static/media/UniversNextPro-Regular.fc95b235.otf"
+  },
+  {
+    "revision": "1dbadcba53c6588910efc359d053f6aa",
+    "url": "/static/media/Vianca_as_Rue_Brooklyn_October_2019.1dbadcba.m4a"
+  },
+  {
+    "revision": "47ee4408b5be75bcf41de0ef04f2300e",
+    "url": "/static/media/Vianca_overlooking_the_city_Brooklyn_July_2019.47ee4408.m4a"
+  },
+  {
+    "revision": "97d60f2c8b6c8a4461b93adf8161f9ec",
+    "url": "/static/media/Wendarlene_at_the_family_photoshoot_Brooklyn_November_2019.97d60f2c.mp4"
+  },
+  {
+    "revision": "66b64958d82f1aecf9e1ef8b247bcaee",
+    "url": "/static/media/WhyteInktrap-Medium.66b64958.otf"
+  },
+  {
+    "revision": "2ef76ed8fd3574eacc50b65e66bd7244",
+    "url": "/static/media/Zion_in_Blue_Harlem_January_2020.2ef76ed8.m4a"
+  },
+  {
+    "revision": "7e0020058cabbb737ad26c45d59c3f0f",
+    "url": "/static/media/Zion_in_Chinatown_Manhattan_December_2019.7e002005.m4a"
+  },
+  {
+    "revision": "eab2d81fada411698083b3220f28c2d7",
+    "url": "/static/media/atlasgrotesk-regular.eab2d81f.ttf"
+  },
+  {
+    "revision": "b589fbe1ceaf0c48f7abd6664fbe5716",
+    "url": "/static/media/atlastypewriter-bold-reduced-webfont.b589fbe1.ttf"
+  },
+  {
+    "revision": "b7456a51363ad29bd67aafba0430f505",
+    "url": "/static/media/cigars-light-webfont.b7456a51.otf"
+  },
+  {
+    "revision": "668475a5df7dc6918c8ae735d32e5679",
+    "url": "/static/media/tiempos.668475a5.otf"
+  },
+  {
+    "revision": "655d7ace4436af7557d71b07438baf80",
+    "url": "/static/media/xime_retrato.655d7ace.PNG"
+  }
+]);
